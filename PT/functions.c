15762e88c651322badac77b00a23d759abbd301d
esdestera <scurtu_estera@yahoo.com>
2016-04-12 17:41:49 +0300

    Update functions.c

--- PT/functions.c
+++ PT/functions.c
@@ -30,3 +30,69 @@ void afisare_vector(int *a, int n){
     }
 }
 
+//linked list :D
+#include <stdio.h>
+#include <stdlib.h>
+#include "function.h"
+struct node {
+    int inf;
+    struct node *next;
+};
+
+void dispay_list(struct node *head){
+
+    if(head->next!=NULL){
+        struct node* current=head;
+        do{
+            current=current->next;
+            printf("%d",current->inf);
+        } while(current->next != NULL);
+        printf("\n");
+    }
+    else
+        printf("the list is empty");
+}
+
+void push_first(struct node *head, int val){
+    struct node *nou;
+    nou=(struct node*)malloc(sizeof (struct node));
+    nou -> inf=val;
+    nou -> next = head -> next;
+    head -> next= nou;
+}
+
+void push_last(struct node *head, int val){
+    struct node *nou;
+    struct node *it=head;
+    nou =(struct node*)malloc(sizeof(struct node) );
+    while(it->next !=NULL)
+            it=it->next;
+    nou->inf=val;
+    it->next= nou;
+    nou-> next =NULL;
+}
+
+int rem_first(struct node *head){
+    struct node *del;
+    int value;
+    del= head->next;
+    value= del->inf;
+    head->next=del->next;
+    free(del);
+    printf("valoarea elementului sters este %d\n", value);
+
+}
+
+void rem_last(struct node *head){
+    struct node *it, *del;
+    int value;
+    it=head;
+    while(it->next->next != NULL)
+        it= it->next;
+    del=it->next;
+    value=del->inf;
+    del->next=NULL;
+    free(del);
+    it->next=NULL;
+    printf("valoarea stearsa este %d\n",value);
+}
