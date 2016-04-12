340e1e766d2e4e4a1b7c74507a27d4ff1cb32fd1
esdestera <scurtu_estera@yahoo.com>
2016-04-12 17:40:49 +0300

    Update functii.h

--- PT/functii.h
+++ PT/functii.h
@@ -6,5 +6,11 @@ void cit_vector(int n, int *a);
 void cit_elem(int n);
 void afisare_vector(int *a, int n)
 
+//liste simplu inlantuite 
+void dispay_list(struct node *head);
+void push_first(struct node *head, int val);
+void push_last(struct node *head, int val);
+void rem_last(struct node *head);
+int rem_first(struct node *head);
 
 #endif // FUNCTII_H_INCLUDED
