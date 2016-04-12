ea74651596b40de85907ecb0bb7fa0624b9d9588
esdestera <scurtu_estera@yahoo.com>
2016-04-12 17:43:02 +0300

    Added files via upload

---
+++ PT/linked_list.c
@@ -0,0 +1,18 @@
+
+#include "function.h "
+int main()
+{
+    struct node *head;
+    head=(struct node*)malloc(sizeof(struct node));
+    head->next=NULL;
+
+    push_first(head,4);
+    push_first(head,5);
+    dispay_list(head);
+    push_last(head,8);
+    dispay_list(head);
+    rem_first(head);
+    rem_last(head);
+    dispay_list(head);
+    return 0;
+}
