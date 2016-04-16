7de77072107ea32ed125185ebb902f8573663687
esdestera <scurtu_estera@yahoo.com>
2016-04-16 12:56:22 +0300

    Update merge.c

--- PT/merge.c
+++ PT/merge.c
@@ -2,8 +2,11 @@
 
 int main()
 {
-    int arr[]={5, 2, 6,1,8,4,0},n;
-    n= sizeof(arr)/sizeof(arr[0]);
+    int arr[200],n;
+    srand(time(NULL));
+    printf("n= ");
+    scanf("%d", &n);
+    random_arr(arr,n);
     afisare_vector(arr,n);
     merge_sort(arr,0,n-1);
     afisare_vector(arr,n);
