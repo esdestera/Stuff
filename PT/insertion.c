ec60e8c9eed6668af6efc564f411656575a285e9
esdestera <scurtu_estera@yahoo.com>
2016-05-20 12:40:01 +0300

    Update insertion.c

--- PT/insertion.c
+++ PT/insertion.c
@@ -10,3 +10,37 @@ int main()
     afisare_vector(a,n);
     return 0;
 }
+
+
+struct bstNode *deleteNode(struct bstNode *root,int key) {
+    struct bstNode * tmp;
+    if (root == NULL) {
+        return root;
+    }
+    if (key < root->data) {
+        root->left = deleteNode(root->left, key);
+    }
+    else if (key > root->data) {
+            root->right = deleteNode(root->right, key);
+    }
+    else {
+
+        if (root->left == NULL) {
+                tmp = root;
+                root = root->right;
+                free(tmp);
+        }
+        else if (root->right == NULL) {
+                    tmp = root;
+                    root = root->left;
+                    free(tmp);
+            }
+        else if (root->left != NULL  && root->right != NULL) {
+                tmp = findMin(root->right);
+                root->data = tmp->data;
+                root->right = deleteNode(root->right, root->data);
+            }
+        }
+
+    return root;
+}
