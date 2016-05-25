6469d44037c57408551f2d20788e36da1cfe4032
esdestera <scurtu_estera@yahoo.com>
2016-05-25 16:24:30 +0300

    Update bst_c.c

--- Proiect BST/bst_c.c
+++ Proiect BST/bst_c.c
@@ -10,44 +10,54 @@
 #include <time.h>
 #include "bst_h.h"
 int randomize();
-struct bstNode{                         // a structure for binary search tree which can be seen as a double linked list
+struct bstNode{
 
-    int data;
-    struct bstNode *left;
-    struct bstNode *right;
+    int data;               /**<information of one node*/
+    struct bstNode *left;   /**<left child of the node*/
+    struct bstNode *right;  /**<right child of the node*/
 };
-struct bstNode *getNode(int key){       // create a node in a tree and return its adress
+/**\struct bstNode bst_h.h bst_c.c
+   \brief This is a structure for Binary Search Trees represented by a linked list with three fields,
+one for information storage, one to point to the left child and one for the right child of the tree
+*/
+struct bstNode *getNode(int key){
     struct bstNode *node = (struct bstNode *) malloc( sizeof ( struct bstNode));
+/**the new node takes the value key*/
     node->data = key;
-    node->left = node->right = NULL;     // because initially the node does not have any children, we make its children to NULL
+/**because initially the node does not have any children, we make its children to NULL*/
+    node->left = node->right = NULL;
     return node;
 }
 
-struct bstNode *insert_bst(struct bstNode *root, int key){     // insert a new node in the tree using recursivity
+struct bstNode *insert_bst(struct bstNode *root, int key){
+/**if the node does not exist we create it;*/
     if(root == NULL){
-        root = getNode(key);                                // if the node does not exist we create it;
+        root = getNode(key);
     }
+/**if the value we want to put in the tree is lesser than the root information, we will insert it in the left subtree*/
     else if( key < root->data){
-        root->left = insert_bst( root->left, key);              //if the value we want to put in the tree is less
-                                                            //or equal than the root information, we will insert
-                                                    //it in the left side of the tree
+        root->left = insert_bst( root->left, key);
     }
+/**otherwise it will be inserted it in the right subtree */
     else{
         root->right = insert_bst( root->right, key);
     }
     return root;
 }
 
-struct bstNode *search_node(struct bstNode *root, int key){             // search for a given value in the tree
+bool search_node(struct bstNode *root, int key){
     if(root == NULL){
         return false;
     }
+/**i found it - the function become true*/
     else if( root->data == key){
-        return root;
+        return true;
     }
-    else if(key <= root->data){
+/**if the value we are looking for is smaller than the root information, we will look for it in the left subtree*/
+    else if(key < root->data){
         return search_node(root->left, key);
     }
+/**otherwise we will look for it in the right subtree */
     else{
         return search_node(root->right, key);
     }
@@ -56,15 +66,67 @@ struct bstNode *search_node(struct bstNode *root, int key){             // searc
 struct bstNode *create_bst(struct bstNode *root, int nrNode){
     int i, data;
     for( i = 0; i < nrNode; i++){
-        data = rand() % 500 + 1;
+/**we insert nodes in the tree using the generation of random numbers*/
+        data = rand() % 900 + 1;
         root = insert_bst(root, data);
     }
 }
 
+int bstSrd(struct bstNode *root){
 
+    if( root == NULL){
+            return 0;
+    }
+/**we use recursivity to traverse the left subtree of the tree as we arrive at the most left leaf, we print it, then go to the
+right side and do the same thing for the right subtree*/
+    bstSrd(root->left);
+    printf("%d\t", root->data);
+    bstSrd(root->right);
 
-/* Print nodes at a given level */
-void printGivenLevel(struct bstNode* root, int level)
+}
+
+int bstRsd(struct bstNode *root){
+    if(root == NULL){
+            return 0;
+    }
+/** we start by printing the root of every visited node, then visit the left subtree and so with the right subtree*/
+    printf("%d\t", root->data);
+    bstRsd(root->left);
+    bstRsd(root->right);
+}
+
+int bstSdr( struct bstNode *root){
+    if(root == NULL){
+            return 0;
+    }
+/**we to traverse the left subtree of the tree as we arrive at the most left leaf, then go to the right side and print its value*/
+        bstSdr(root->left);
+        bstSdr(root->right);
+        printf("%d\t",root->data);
+
+}
+
+int height(struct bstNode* root)
+{
+    if (root==NULL)
+        return 0;
+    else
+    {
+/** compute the height of each subtree */
+        int leftHeight = height(root->left);
+        int rightHeight = height(root->right);
+
+/** use the larger one */
+        if (leftHeight > rightHeight){
+            return(leftHeight+1);
+        }
+        else{
+                return (rightHeight+1);
+        }
+    }
+}
+
+int printGivenLevel(struct bstNode* root, int level)
 {
     if (root == NULL)
         return;
@@ -78,29 +140,6 @@ void printGivenLevel(struct bstNode* root, int level)
     }
 }
 
-/* Compute the "height" of a tree -- the number of
-    nodes along the longest path from the root node
-    down to the farthest leaf node.*/
-int height(struct bstNode* node)
-{
-    if (node==NULL)
-        return 0;
-    else
-    {
-        /* compute the height of each subtree */
-        int leftHeight = height(node->left);
-        int rightHeight = height(node->right);
-
-        /* use the larger one */
-        if (leftHeight > rightHeight){
-            return(leftHeight+1);
-        }
-        else{
-                return (rightHeight+1);
-        }
-    }
-}
-/* Print nodes at a given level */
 void printLevelOrder(struct bstNode* root)
 {
     int h = height(root);
@@ -111,87 +150,75 @@ void printLevelOrder(struct bstNode* root)
     }
 }
 
-struct bstNode *bstSrd(struct bstNode *root){           //in-order traversal SRD;
-
-    if( root == NULL){
-            return 0;
-    }
-    bstSrd(root->left);                         //we use recursivity to traverse the left subtree of the tree
-                                                // as we arrive at the most left leaf, we print it, go to the
-                                                //right side and do the same thing for the right subtree
-    printf("%d\t", root->data);
-    bstSrd(root->right);
-
-}
-
-struct bstNode *bstRsd(struct bstNode *root){           //preorder traversal RSD
+struct bstNode *findMin(struct bstNode *root){
+/**if the tree is empty the function will return 0;*/
     if(root == NULL){
-            return 0;
-    }
-    printf("%d\t", root->data);                 // we start by printing the root of every visited node,
-                                            //then visit the left subtree and so with the right subtree
-    bstRsd(root->left);
-    bstRsd(root->right);
-}
-
-struct bstNode *bstSdr( struct bstNode *root){          //postorder traversal SDR
-    if(root == NULL){
-            return 0;
-    }
-        bstSdr(root->left);
-        bstSdr(root->right);
-        printf("%d\t",root->data);
-
-}
-
-struct bstNode *findMin(struct bstNode *root){      // return the minimum value in the tree
-    if(root == NULL){                               // if the tree is empty the function will return 0;
         printf("the tree is empty");
         return 0;
     }
-    else if( root->left == NULL){                   // else,we will move in the left subtree while there is a left child,
+/**else,we will move in the left subtree while there is a left child*/
+    else if( root->left == NULL){                   // ,
         return root;
     }
 
     return findMin(root->left);
 }
 
-struct bstNode *findMax(struct bstNode *root){         // return the highest value in the tree
-    if(root == NULL){
-        printf("the tree is empty");
-        return 0;
-    }
-    else if(root->right == NULL){                       // else,we will move in the right subtree while there is a right child,
-        return root;
-    }
-
-    return findMax(root->right);
-
-}
-
 struct bstNode *deleteNode(struct bstNode *root,int key) {
     struct bstNode * temp;
+/** if the tree is empty the function will return the root address */
     if (root == NULL) {
         return root;
     }
+/**if the value is smaller than the value of the root, then the waned value will be searched in the left subtree*/
     if (key < root->data) {
         root->left = deleteNode(root->left, key);
     }
+/**if the value is greater than the value of the root, then the waned value will be searched in the right subtree*/
     else if (key > root->data) {
             root->right = deleteNode(root->right, key);
     }
-    else {
+/**we found the wanted value and three cases can be distinguished
+        -# Case 1 - the node doesn't have any child
+        -# Case 2 - the node has only one child
+        -# Case 3 - the node has both left and right children
+*/
+/**\brief Case 1: no children
 
-        if (root->left == NULL) {
+   * we simply deallocate the node from memory using free function and make it points to NULL*/
+    else {
+        if(root->left == NULL && root->right == NULL){
+            free(root);
+            root = NULL;
+        }
+/**\brief Case 2: one child
+
+ * there can be 2 situations, the node can have either the left node or a right node, the algorithm is similar for both of them
+ */
+ /**
+ -if the node has a right child, we will use a auxiliary variable which will take the address of the node to be deleted while the
+ node become its right child, the only thing that remains to be done is to deallocate the memory stored in the temporary variable
+ */
+        else if (root->left == NULL) {
                 temp = root;
                 root = root->right;
                 free(temp);
         }
+ /**
+ -if the node has a left child, we will use a auxiliary variable which will take the address of the node to be deleted while the
+ node become its left child, the only thing that remains to be done is to deallocate the memory stored in the temporary variable
+ */
         else if (root->right == NULL) {
                     temp = root;
                     root = root->left;
                     free(temp);
             }
+/**\brief Case 3 - two children
+
+*we will use the function  findMin to find the minimum value in the right subtree, its address will be stored in the variable temp
+next step is to copy the value of the minimum value in the node that has the wanted value.Now we have two nodes with the same value and
+we can be in any case described above so we have to recall the function  deleteNode with the parameters root->right, the right child
+of the current node and root->data */
         else if (root->left != NULL  && root->right != NULL) {
                 temp = findMin(root->right);
                 root->data = temp->data;
@@ -206,87 +233,11 @@ void freeTree(struct bstNode *root) {
     if (root == NULL) {
         return;
     }
+/**delete the left subtree*/
     freeTree(root->left);
+/**delete the right subtree*/
     freeTree(root->right);
+/**delete the root*/
     free(root);
 }
-
-/*
-struct bstNode *deleteNode(struct bstNode *root,int key)
-{
-    struct bstNode *find = search_node(root, key);
-    struct bstNode *temp;
-    if(root == NULL ){
-        return 0;
-    }
-    if(find == NULL){
-        return find;
-    }
-    else if(find->left == NULL && find->right == NULL){
-        free(find);
-        find == NULL;
-
-    }
-    else if(find->left != NULL && find->right == NULL){
-
-        temp = find;
-        find = find->left;
-        free(temp);
-    }
-    else if(find->left == NULL && find->right != NULL){
-        temp = find;
-        find = find->right;
-        free(temp);
-    }
-    else if(find->left != NULL && find->right != NULL){
-        struct bstNode *min = findMin(find->right);
-        find->data = min->data;
-        find->right = deleteNode(find->right,min->data);
-    }
-    return find;
-}
-*/
-/*struct bstNode *delete_node(struct bstNode *root, int key){     // delete the node that has a value we want
-    if(search_bst(root, key) == false ){
-        printf("the value has not been find!");
-        return 0;
-    }
-    if( root == NULL ){                           //if the tree is empty the function will return the root address
-        return root;
-    }
-    else if ( key < root->data ){               //if the value is less than the value of the root,
-                                                //then the waned value will be searched in the left subtree
-        root->left = delete_node(root->left,key);
-    }
-    else if( key > root->data){                 //if the value is bigger than the value of the root,
-                                                //then the waned value will be searched in the left subtree
-        root->right = delete_node(root->right,key);
-    }
-    else{                               // the value has been find and there may be three cases
-
-        if(root->left == NULL && root->right == NULL){  //if the node does not have any children we delete the node
-            free (root);                                //logical deletion
-            root = NULL;
-        }
-        else if(root->left == NULL ){           // if the node has only  right children, we will use an auxiliary variable
-            struct bstNode *aux = root;         // it will store the address of the wanted node
-            root = root->right;                 // the node that has the value we want become the right child that the node has
-            free(aux);                          // the node will be deleted
-        }
-        else if(root->right == NULL){           // if the node does not have any right children, we will use an auxiliary variable
-            struct bstNode *aux = root;         // the auxiliary variable  will store the address of the wanted node
-            root = root->left;                  // the node that has the value we want become the left child that the node has
-            free(aux);                           // the node will be deleted
-        }
-        else{
-            struct bstNode *aux = findMin(root->right);         // we are looking for the minimum value in the
-                                                        //right subtree, and we store it in the variable aux;
-            root->data = aux->data;                         //we put the value we find in place of the value we want to delete
-            root->right = delete_node(root->right,aux->data);     //we delete the node that fist had the minimum value
-        }
-       return root;
-    }
-}
-*/
-
 
