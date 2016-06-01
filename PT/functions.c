a91f7f0e0c034e90912a68660936bc420deac8f4
esdestera <scurtu_estera@yahoo.com>
2016-06-01 23:33:28 +0300

    Update functions.c

--- PT/functions.c
+++ PT/functions.c
@@ -547,3 +547,35 @@ bool solveNQ()
     printSolution(board);
     return true;
 }
+
+//knapsack
+int max(int a, int b) {
+  if(a > b)
+    return a;
+  else
+    return b;
+}
+
+// Returns the maximum value that can be put in a knapsack of capacity Weight
+int knapSack(int weight, int wt[], int val[], int n)
+{
+   int i, j;
+   int K[n+1][weight+1];
+
+   // Build table K[][] in bottom up manner
+   for (i = 0; i <= n; i++)
+   {
+       for (j = 0; j <= weight; j++)
+       {
+           if (i==0 || j==0)
+               K[i][j] = 0;
+           else if (wt[i-1] <= j)
+                 K[i][j] = max(val[i-1] + K[i-1][j-wt[i-1]],  K[i-1][j]);
+           else
+                 K[i][j] = K[i-1][j];
+       }
+   }
+
+   return K[n][weight];
+}
+
